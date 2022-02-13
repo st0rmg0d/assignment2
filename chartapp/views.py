dc374590829c2f7fa171315a04aeeddfe06a4561
st0rmg0d <55540414+st0rmg0d@users.noreply.github.com>
2022-02-13 13:39:23 +0600

    Update views.py

--- chartapp/views.py
+++ chartapp/views.py
@@ -60,25 +60,7 @@ def pie(request):
         "products": products,
         "form": form
     }
-    # url = 'https://etherscan.io/accounts/1?ps=100'
-    # req = Request(url, headers={
-    # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'})
-    # response = urlopen(req, timeout=20).read()
-    # response_close = urlopen(req, timeout=20).close()
-    # page_soup = soup(response, "html.parser")
-    # Transfers_info_table_1 = page_soup.find(
-    #     "div", {"class": "table-responsive mb-2 mb-md-0"})
-    # df = pd.read_html(str(Transfers_info_table_1))[0]
-    # df.to_csv("TransferTable.csv", index=False)
-    # tmp_data=pd.read_csv('TransferTable.csv',sep=';')
-    # products = [
-    #     Product(
-    #         name = tmp_data.ix[row]['Address'],
-    #         price = tmp_data.ix[row]['Balance'],
-    #     )
-    #     for row in tmp_data['Rank']
-    # ]
-    # Product.objects.bulk_create(products)
+
 
     return render(request, 'chartapp/pie.html', context)
 
@@ -98,24 +80,5 @@ def line(request):
         "products": products,
         "form": form
     }
-    # url = 'https://etherscan.io/accounts/1?ps=100'
-    # req = Request(url, headers={
-    # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'})
-    # response = urlopen(req, timeout=20).read()
-    # response_close = urlopen(req, timeout=20).close()
-    # page_soup = soup(response, "html.parser")
-    # Transfers_info_table_1 = page_soup.find(
-    #     "div", {"class": "table-responsive mb-2 mb-md-0"})
-    # df = pd.read_html(str(Transfers_info_table_1))[0]
-    # df.to_csv("TransferTable.csv", index=False)
-    # tmp_data=pd.read_csv('TransferTable.csv',sep=';')
-    # products = [
-    #     Product(
-    #         name = tmp_data.ix[row]['Address'],
-    #         price = tmp_data.ix[row]['Balance'],
-    #     )
-    #     for row in tmp_data['Rank']
-    # ]
-    # Product.objects.bulk_create(products)
 
     return render(request, 'chartapp/line.html', context)
